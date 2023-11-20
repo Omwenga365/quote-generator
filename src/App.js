30e2ba07513f8097e0022d012898acfba9aa49b3
Omwenga365 <victoromwenga365@gmail.com>
2023-11-20 21:57:24 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
-import './App.css';
+// App.js
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Favorites from './Favorites';
 
 const App = () => {
-  const url = "https://api.quotable.io/random";
-  let quoteData = {
-    content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  };
-  const [quote, setQuote] = useState(quoteData);
-
-  const generateQuote = () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setQuote(data);
-      });
-  };
-
-  const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content);
-    alert('Copied');
-  };
-
-  
-  const shareOnWhatsApp = () => {
-    const text = encodeURIComponent(quote.author + " once said: " + quote.content);
-    window.open(`https://api.whatsapp.com/send?text=${text}`);
-  };
-
-  
-  const shareOnTwitter = () => {
-    const text = encodeURIComponent(quote.author + " once said: " + quote.content);
-    window.open(`https://twitter.com/intent/tweet?text=${text}`);
-  };
+  // ... (your existing code)
 
   return (
-    <>
-      <h1>Quote Generator React App</h1>
-      <div className="container">
-        <p>{quote.content}</p>
-        <span>{quote.author}</span>
-        <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
-          <button onClick={generateQuote}>Generate Another Quote</button>
-          <button onClick={shareOnWhatsApp}>Share on WhatsApp</button>
-          <button onClick={shareOnTwitter}>Share on Twitter</button>
-        </div>
-      </div>
-    </>
+    <Router>
+      <>
+        <h1>Quote Generator React App</h1>
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <Home
+              {...props}
+              quote={quote}
+              generateQuote={generateQuote}
+              copy={copy}
+              shareOnWhatsApp={shareOnWhatsApp}
+              shareOnTwitter={shareOnTwitter}
+            />
+          )}
+        />
+        <Route path="/favorites" component={Favorites} />
+      </>
+    </Router>
   );
 };
 
---
+++ src/components/Favorites.js
@@ -0,0 +1,15 @@
+// Favorites.js
+import React from 'react';
+
+const Favorites = () => {
+  // Fetch and display favorite quotes from localStorage or any other source
+  // You can add more functionalities like removing favorites or displaying details
+  return (
+    <div className="container">
+      <h2>Favorites</h2>
+      <p>This is where your favorite quotes will be displayed.</p>
+    </div>
+  );
+};
+
+export default Favorites;
