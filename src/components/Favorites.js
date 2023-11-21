51049cda53f153a72881ec74095360fc4e5cf6bf
Omwenga365 <victoromwenga365@gmail.com>
2023-11-21 22:40:18 +0300

    done

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,13 +1,14 @@
 // Favorites.js
 import React from 'react';
+import './Favorites.css'; // Import your stylesheet
 
 const Favorites = () => {
   // Fetch and display favorite quotes from localStorage or any other source
   // You can add more functionalities like removing favorites or displaying details
   return (
-    <div className="container">
-      <h2>Favorites</h2>
-      <p>This is where your favorite quotes will be displayed.</p>
+    <div className="favorites-container">
+      <h2 className="favorites-heading">Favorites</h2>
+      <p className="favorites-description">This is where your favorite quotes will be displayed.</p>
     </div>
   );
 };
