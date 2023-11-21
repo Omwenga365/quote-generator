442484ad523e503dbe36f8bc23ad86a64a48ffe0
Omwenga365 <victoromwenga365@gmail.com>
2023-11-21 22:43:45 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 // Home.js
 import React, { useEffect } from 'react';
+import './Home.css'; // Import your stylesheet
 
 const Home = ({ quote, generateQuote, copy, shareOnWhatsApp, shareOnTwitter }) => {
   useEffect(() => {
@@ -10,17 +11,17 @@ const Home = ({ quote, generateQuote, copy, shareOnWhatsApp, shareOnTwitter }) =
   if (!quote) {
     // If quote is undefined, display a loading message
     return (
-      <div>
+      <div className="loading-message">
         Loading quote...
       </div>
     );
   }
 
   return (
-    <div>
-      <p>{quote.content}</p>
-      <span>{quote.author}</span>
-      <div>
+    <div className="quote-container">
+      <p className="quote-content">{quote.content}</p>
+      <span className="quote-author">{quote.author}</span>
+      <div className="button-container">
         <button onClick={copy}>Copy</button>
         <button onClick={generateQuote}>Generate Another Quote</button>
         <button onClick={shareOnWhatsApp}>Share on WhatsApp</button>
